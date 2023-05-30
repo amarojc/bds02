7e9a084f986361f66702b6935dd89fcb3a30b05b
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-05-29 21:45:22 -0300

    Test update passing, returning Event not found when non existing id.

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.bds02.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,7 @@ import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class EventService {
@@ -22,11 +25,15 @@ public class EventService {
 	
 	@Transactional
 	public EventDTO update(Long id, EventDTO eventDTO) {
-		Event event = eventRepository.getOne(id);
-		copyEventDtoToEntityEvent(eventDTO, event);
-		event = eventRepository.save(event);
-
-		return new EventDTO(event);
+		try {
+			Event event = eventRepository.getOne(id);
+			copyEventDtoToEntityEvent(eventDTO, event);
+			event = eventRepository.save(event);
+			
+			return new EventDTO(event);
+		}catch(EntityNotFoundException ex) {
+			throw new ObjectNotFoundException("Evento não encontrado / Event not found " + id);
+		}
 	}
 		
 	private void copyEventDtoToEntityEvent(EventDTO eventDTO, Event event) {
